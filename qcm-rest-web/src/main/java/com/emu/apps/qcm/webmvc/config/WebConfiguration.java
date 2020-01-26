92dc52658db7e254cb53de949259f1d2fa6f3074
Eric MULLER <eric.pierre.muller@gmail.com>
2020-01-26 22:22:22 +0100

    cors issue

--- qcm-rest-web/src/main/java/com/emu/apps/qcm/webmvc/config/WebConfiguration.java
+++ qcm-rest-web/src/main/java/com/emu/apps/qcm/webmvc/config/WebConfiguration.java
@@ -68,8 +68,8 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-                //.allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**").allowedOrigins("*").
+                allowedMethods("GET", "POST", "PUT", "OPTIONS","DELETE");
     }
 
 }
